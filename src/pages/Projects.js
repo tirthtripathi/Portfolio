e2cec2eb582efd180e781fa97807b92c33c90072
Tirth Tripathi <tirthtripathi13@gmail.com>
2024-06-15 18:39:43 +0530

    new project added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import img1 from "../assets/projects/Project1.webp";
 import img2 from "../assets/projects/Project2.webp";
+// import img3 from
 import GitHubIcon from "@mui/icons-material/GitHub";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
@@ -84,6 +85,38 @@ export default function Projects({ setModal, setProjectNo }) {
               </IconButton>
             </Tooltip>
           </div>
+        </div>
+      </div>
+
+      <div className="flex max-[940px]:flex-col mt-10 gap-4">
+        <div className="relative max-[940px]:flex max-[940px]:justify-start">
+          <img
+            alt="Project"
+            className="h-80 w-max max-[1500px]:h-64 max-[1140px]:h-52 max-[455px]:h-44 rounded-xl"
+            src={img1}
+          ></img>
+          <div className="absolute bottom-0 max-[800px]:hidden  left-0">
+            <Tooltip title="View Repository">
+              <IconButton
+                onClick={navigateToLinksHandler.bind(
+                  this,
+                  "https://github.com/tirthtripathi/Chatapp"
+                )}
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </div>
+        <div className="relative max-[940px]:flex max-[940px]:justify-end">
+         <a href="https://panchayat-frn1.onrender.com/" target="_blank"><h1 className=" underline underline-offset-2 font-semibold text-2xl text-yellow-400">Panchayat</h1></a> 
+          <p>Real time Chat app using MERN stack, Socket.io and ChakraUI</p><br/>
+          <ul>
+            <li>Real-timechat <b className=" text-yellow-500">(one-to-one and groupchats)</b></li>
+            <li>Token based <b className=" text-yellow-500"> User authentication</b> using <b className=" text-yellow-500">JWT</b> .</li>
+            <li>Admin capabilities to <b className=" text-yellow-500">add/remove members</b> from groups.</li>
+            <li><b className=" text-yellow-500">Real-time notifications</b> and <b className=" text-yellow-500">typing status</b> for users.</li>
+          </ul>
         </div>
       </div>
     </div>
